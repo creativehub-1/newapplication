077159397dc82052a154325cf995f8162f2b7112
Yanagandula <venu.yanagandula@capgemini.com>
2026-01-08 14:11:27 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace newapplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
